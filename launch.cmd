ce7ae5c5d04146032dfa17c1cd225d741476269b
Ayane Satomi <sr229@coder.com>
2019-12-03 14:31:53 +0800

    Initial Commit
    Signed-off-by: Ayane Satomi <sr229@coder.com>

---
+++ launch.cmd
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+SETLOCAL
+
+SET "NODE_DIST=node-v8.12.0"
+SET "APP_MAIN_JS=app.js"
+
+SET "NODE_EXE=%~dp0\%NODE_DIST%-win*\node.exe"
+SET "NODE_PATH=%~dp0\%NODE_DIST%-win*\node_modules"
+SET "APP_MAIN_JS_PATH=%~dp0\%APP_MAIN_JS%"
+
+CALL %NODE_EXE% %APP_MAIN_JS_PATH% %*
+EXIT /B %ERRORLEVEL%
