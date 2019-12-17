2b73198c5323fb4738ca8947c5a69176278f1687
Ayane Satomi <sr229@coder.com>
2019-12-18 04:10:35 +0800

    upgrade to node12

--- launch.cmd
+++ launch.cmd
@@ -3,7 +3,7 @@
 SETLOCAL
 
 SET "NODE_DIST=node-v10.17.0"
-SET "APP_MAIN_JS=app.js"
+SET "APP_MAIN_JS=app\out\vs\server\main.js"
 
 SET "NODE_EXE=%~dp0\%NODE_DIST%-win*\node.exe"
 SET "NODE_PATH=%~dp0\%NODE_DIST%-win*\node_modules"
